40c8aa0ae9e96e3703a70e80ce0b20041577ca18
vinod-student90 <vinodmca851@gmail.com>
2024-09-18 22:11:49 +0530

    Add files via upload
    testbrowser launch

---
+++ chrome.java
@@ -0,0 +1,14 @@
+package muthootworld;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver =new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+	}
+
+}
